8e05ab7e0bf19861fadc5d28e7241da295b9837b
Marcin Skladanowski <marcin.skladanowski@accenture.com>
2022-07-24 12:49:40 +0200

    list all owners

--- force-app/main/default/lwc/owners/owners.js
+++ force-app/main/default/lwc/owners/owners.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Car_Showrooms_Owner__c.Name';
+import getOwners from '@salesforce/apex/OwnerController.getOwners';
+const COLUMNS = [
+    {label: 'Name', fieldName: NAME_FIELD.fieldApiName, type: 'text'}
+];
 
-export default class Owners extends LightningElement {}
+export default class Owners extends LightningElement {
+    columns = COLUMNS
+    @wire(getOwners)
+    owners;
+}
