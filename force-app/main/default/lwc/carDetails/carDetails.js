89e8593ea9ac59b1e49fc67b34cab6e30a353999
Marcin Skladanowski <marcin.skladanowski@accenture.com>
2022-07-26 11:41:52 +0200

    wipe comments and console.logs

--- force-app/main/default/lwc/carDetails/carDetails.js
+++ force-app/main/default/lwc/carDetails/carDetails.js
@@ -2,27 +2,13 @@ import { LightningElement, api, track } from 'lwc';
 import getCarDetails from '@salesforce/apex/DataCollector.getCarDetails';
 
 export default class CarDetails extends LightningElement {
-    // @api chosencar;
     @api askedForDetails = false;
     @api passed;
     @api details;
 
     @api
     askForCarDetails(event){
-        // console.log('company event invoked')
-        // this.title = "Select your showroom"
         this.askedForDetails = true
-        console.log('askedForDetails: ', this.askedForDetails)
-        console.log('passed Id - value: ', this.passed)
-
-        // console.log('companySelected = ', this.companySelected)
-        // let id = event.currentTarget.dataset.id
-        // console.log('received id = ', id)
-        // console.log('company chosen = ', getCompanyName({owner: id}))
-        
-        // getCarDetails({car: this.chosenCar })
-        // .then(result => {this.details = result})
-        // console.log('ilośc salonów = ', this.chosenShowrooms.length)
     }
 
     
--- force-app/main/default/lwc/selectCar/selectCar.js
+++ force-app/main/default/lwc/selectCar/selectCar.js
@@ -2,11 +2,8 @@ import { LightningElement, track, api} from 'lwc';
 import getOnwersRecords from '@salesforce/apex/DataCollector.getOnwersRecords';
 import getShowrooms from '@salesforce/apex/DataCollector.getShowrooms';
 import getCars from '@salesforce/apex/DataCollector.getCars';
-// import getCompanyName from '@salesforce/apex/DataCollector.getCompanyName';
-
 
 export default class SelectCar extends LightningElement {
-    // @api title = "Select your company";
     @api companySelected = false
     @api showroomSelected = false
     @api receivedId = false;
@@ -16,42 +13,28 @@ export default class SelectCar extends LightningElement {
     @track chosenShowrooms;
     @track chosenCars;
     
-    // @api recordId;
 
     constructor(){
         super();
         getOnwersRecords()
         .then(result => {this.Owners = result})
-        // console.log('owners:', Owners);
     }
 
     askForShowrooms(event){
-        // console.log('company event invoked')
-        // this.title = "Select your showroom"
         this.companySelected = true
         this.showroomSelected = false
-        // console.log('companySelected = ', this.companySelected)
         let id = event.currentTarget.dataset.id
-        // console.log('received id = ', id)
-        // console.log('company chosen = ', getCompanyName({owner: id}))
         
         getShowrooms({owner: id })
         .then(result => {this.chosenShowrooms = result})
-        // console.log('ilośc salonów = ', this.chosenShowrooms.length)
     }
 
     askForCars(event){
-        // console.log('showroom event invoked')
-        // this.title = "Select your car"
         this.showroomSelected = true
-        // console.log('showroomSelected = ', this.showroomSelected)
         let id = event.currentTarget.dataset.id
-        // console.log('received id = ', id)
-        // console.log('company chosen = ', getCompanyName(id))
         
         getCars({showroom: id })
         .then(result => {this.chosenCars = result})
-        // console.log('ilośc salonów = ', this.chosenShowrooms.length)
     }
 
     passCarId(event){
@@ -60,6 +43,4 @@ export default class SelectCar extends LightningElement {
         console.log('chosenCarId: ', this.chosenCarId)
         this.template.querySelector('c-car-details').askForCarDetails();
     }
-
-    //dodać eventHandler przy kliklnięciu auta, żeby otrzyamć jego Id do przekazania do dziecka
 }
