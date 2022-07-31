a45345ef3252c066134de3f9198163d47ef6cf01
Marcin Skladanowski <marcin.skladanowski@accenture.com>
2022-07-31 14:54:44 +0200

    refactor spinner

--- force-app/main/default/lwc/carDetails/carDetails.js
+++ force-app/main/default/lwc/carDetails/carDetails.js
@@ -1,5 +1,5 @@
 import { LightningElement, api, track } from 'lwc';
-import getCarDetails from '@salesforce/apex/DataCollector.getCarDetails';
+// import getCarDetails from '@salesforce/apex/DataCollector.getCarDetails';
 
 export default class CarDetails extends LightningElement {
     @api isAskedForDetails = false;
--- force-app/main/default/lwc/selectCar/selectCar.js
+++ force-app/main/default/lwc/selectCar/selectCar.js
@@ -21,7 +21,9 @@ export default class SelectCar extends LightningElement {
     @track chosenShowrooms;
     @track chosenCars;
 
+    _toastTitle;
     _toastMessage;
+
 
     carLogo = carsForSale;
     
@@ -39,53 +41,88 @@ export default class SelectCar extends LightningElement {
     }
 
     askForShowrooms(event){
-        this.isLoading = true;
-        let id = event.currentTarget.dataset.id;
-
-        getShowrooms({outOwner: id })
-        .then(result => {
-
-            {this.isShowroomSelected = false};
-            {this.isNoCars = false};
-            
-            if (result.length > 0) {
-                {this.isCompanySelected = true};
-                // {this.isShowroomSelected = false};
-                {this.isNoShowrooms = false};
-                // {this.isNoCars = false};
-                {this.chosenShowrooms = result};
-            }
-            // else {
-            //     {this.isCompanySelected = false};
-            //     // {this.isShowroomSelected = false};
-            //     {this.isNoShowrooms = true};
-            //     // {this.isNoCars = false};
-            // }
-        })
-        .catch(error => {
-            {this.isCompanySelected = false};
-            // {this.isNoShowrooms = true};
-            this._toastMessage = 'No showroms owned by selected company.'
-            this.showToast()
-
-            // console.error('Error - getShowrooms() for company Id = ' + id);
-        })
         
+        // console.log('askForShowrooms testing spinner')
+        this.isLoading = true;
+        // console.log('askForShowrooms isLoading: ', this.isLoading);
+        let id = event.currentTarget.dataset.id;
+        setTimeout(() =>         
+            getShowrooms({outOwner: id })
+            .then(result => {
+                // console.log('getShowrooms called');
+                // console.log('isShowroomSelected: ', this.isShowroomSelected);
+
+                {this.isShowroomSelected = false};
+                // console.log('isShowroomSelected: ', this.isShowroomSelected);
+                
+                // console.log('isNoCars: ', this.isNoCars);
+                {this.isNoCars = false};
+                // console.log('isNoCars: ', this.isNoCars);
+                
+                // console.log('result.length: ', result.length);
+                if (result.length > 0) {
+                    // console.log('if >=0: isCompanySelected: ', this.isCompanySelected);
+                    {this.isCompanySelected = true};
+                    // console.log('if >=0: isCompanySelected: ', this.isCompanySelected);
+                    // {this.isShowroomSelected = false};
+                    // console.log('if >=0: isNoShowrooms', this.isNoShowrooms);
+                    {this.isNoShowrooms = false};
+                    // console.log('if >=0: isNoShowrooms', this.isNoShowrooms);
+                    // {this.isNoCars = false};
+                    // console.log('if >=0: chosenShowrooms:', this.chosenShowrooms);
+                    {this.chosenShowrooms = result};
+                    // console.log('if >=0: chosenShowrooms:', this.chosenShowrooms);
+                    {this.isLoading = false};
+                    console.log('getShowrooms isLoading: ', this.isLoading);
+                }
+                // else {
+                //     {this.isCompanySelected = false};
+                //     // {this.isShowroomSelected = false};
+                //     {this.isNoShowrooms = true};
+                //     // {this.isNoCars = false};
+                // }
+
+            })
+            .catch(error => {
+                // console.log('entered .catch');
+                // console.log('.catch: isCompanySelected: ', this.isCompanySelected);
+
+                {this.isCompanySelected = false};
+                // console.log('.catch: isCompanySelected: ', this.isCompanySelected);
+
+                // {this.isNoShowrooms = true};
+                {this._toastTitle = 'Company selection:'};
+                // console.log('_toastMessage:', this._toastMessage);
+                {this._toastMessage = 'No showroms owned by selected company.'};
+                // console.log('_toastMessage:', this._toastMessage);
+                this.showToast();
+                {this.isLoading = false};
+
+
+                // console.error('Error - getShowrooms() for company Id = ' + id);
+            })
+        , 1500);
+
         this.isAskedForDetails = false;
         // this.template.querySelector('c-car-details').hideCArDetails();
-        this.isLoading = false;
+
     }
 
     askForCars(event){
+        // console.log('askForCars isLoading start: ', this.isLoading);
         this.isLoading = true;
+        console.log('askForCars isLoading changed: ', this.isLoading);
         let id = event.currentTarget.dataset.id;
-
+        
+        setTimeout(()=>
         getCars({outShowroom: id })
         .then(result => {
             if (result.length >= 1) {
                 {this.isNoCars = false};
-                this.isShowroomSelected = true;
+                {this.isShowroomSelected = true};
                 {this.chosenCars = result};
+                {this.isLoading = false};
+                console.log('askForCars isLoading: ', this.isLoading);
             // } else {
             //     {this.isShowroomSelected = false};
             //     {this.isNoCars = true};
@@ -93,17 +130,17 @@ export default class SelectCar extends LightningElement {
         })
         .catch(error => {
             {this.isShowroomSelected = false};
+            {this._toastTitle = 'Showroom selection:'}
             // {this.isNoCars = true};
-            this._toastMessage = 'No cars stored in selected showroom.'
+            {this._toastMessage = 'No cars stored in selected showroom.'}
             this.showToast()
-
+            this.isLoading = false;
 
             // console.log('Error - getCars() for showroom Id = ' + id);
         })
-
+        , 1500);
         // this.template.querySelector('c-car-details').hideCArDetails();
         this.isAskedForDetails = false;
-        this.isLoading = false;
     }
 
     passCarId(event){
@@ -120,10 +157,10 @@ export default class SelectCar extends LightningElement {
 
     showToast() {
         const event = new ShowToastEvent({
-            title: 'Ekhm...',
+            title: this._toastTitle,
             message: this._toastMessage,
             variant: 'error',
-            mode: 'sticky'
+            // mode: 'sticky'
         });
         this.dispatchEvent(event);
     }
